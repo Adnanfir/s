e9d5f0819e725f4ea14a9bd00f7b13b166f3555c
Adnan Firdous <agaadnanfirdous@gmail.com>
2025-05-02 10:25:06 +0530

    Prime numbers revised

--- prime.c
+++ prime.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main(){
     int i;
     int target;
     printf("Enter the Target Value : ");
     scanf("%d",&target);
-
+    int p=0;
+bool isprime=true;
 for (  i = 2; i <target; i++)
 {
     if ((target%i)==0)
     {
-        printf("The Number is a %d Consonant",target);
-        break;
-    }
-    else
-    {
-        printf("The Number %d is a prime",target);
-        break;
+        isprime=false;
+        p++;
+        
     }
     
     
 }
-
+if (isprime==true)
+{
+    printf("The number is prime\n");
+}
+else
+{
+    printf("the number is Non-prime\n");
+}
+printf("Total number  of divisors for the Number %d ",p);
 
 
 
