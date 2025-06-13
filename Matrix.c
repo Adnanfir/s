4fdb56384798791e2240bd44dd2d124b875e21b7
Adnan fir <agaadnanfirdous@gmail.com>
2025-06-13 11:51:27 +0530

    .\sum_of_elements_of_an_array

--- Matrix.c
+++ Matrix.c
@@ -16,19 +16,39 @@ int main()
         for (int j = 0; j < SIZE; j++)
         {
             scanf("%s", &Matrix[i][j]);
-            Matrix[i][j+1]='_';
-
-            for (int i = 0; i < SIZE; i++)
-            {
-                printf("|");
-                for (int j = 0; j < SIZE; j++)
-                {   
-                    printf("%c", Matrix[i][j]);
-                    
-                }
-                printf("|");
-                printf("\n");
-            }
         }
     }
+    for (int i = 0; i < SIZE; i++)
+    {
+        // printf("|");
+        if (i == 0)
+        {
+            for (int j = SIZE - 1; j >= 0; j--)
+            {
+                printf("%c", Matrix[i][j]);
+            }
+
+            printf("\n");
+        }
+        else if (i == 1)
+        {
+            {   for (int j = 0; j <1; j++)
+            {
+                printf("%c", Matrix[i][j]);
+            }              
+            }
+            printf("\n");
+        }
+        else if (i == 2)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                printf("%c", Matrix[i][j]);
+            }
+        }
+
+        // Matrix[i][j+1]='_';
+    }
+    // printf("|");
+    printf("\n");
 }
